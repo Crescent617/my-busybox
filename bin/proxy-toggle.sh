fd8d19227179746381e06a758093da83a0528690
Crescent617 <lihuaru617@outlook.com>
2023-07-09 11:23:59 +0800

    add uppercase

--- bin/proxy-toggle.sh
+++ bin/proxy-toggle.sh
@@ -7,10 +7,19 @@ if [[ -n $http_proxy ]]; then
   unset http_proxy
   unset https_proxy
   unset no_proxy
+
+  unset HTTP_PROXY
+  unset HTTPS_PROXY
+  unset NO_PROXY
 else
   echo 'enable http_proxy'
   proxy='http://localhost:7890'
+
   export http_proxy=$proxy
   export https_proxy=$proxy
   export no_proxy="localhost, 127.0.0.1, ::1"
+
+  export HTTP_PROXY=$proxy
+  export HTTPS_PROXY=$proxy
+  export NO_PROXY="localhost, 127.0.0.1, ::1"
 fi
